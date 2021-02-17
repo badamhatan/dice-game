cc65882899c4f984d251af6d71dc4e72c521e844
“BAXAA” <b_badamhatan@yahoo.com>
2021-02-17 13:52:19 +0800

    New Button belen bolgox

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-//Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийг 0, 2-р тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 0;
+// Тоглоомны бүх газарт ашиглагдах глобаль хувьсагчдыг энд зарлая
 
-// Тоглогчдын оноог хадгалдаг хувьсагч
-var scores = [0, 0];
-// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-var roundScore = 0;
+//Аль тоглогч шоо шидэх вэ гэдгийг энд хадгална.
+var activePlayer;
+// 2 тоглогчийн цуглуулсан оноонууд
+var scores;
+// Идэвхитэй тоглогчийн цуглуулж байгаа ээлжийн оноо.
+var roundScore;
 
-// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-// Math.floor() - Тоог доош нь тоймлоно. Ж.нь: 1,1=1   2.8=2
-// Math.random - 0-1 хүртэл санамсаргүйгээр тоог гаргана
-var diceNumber = Math.floor(Math.random() * 6) + 1;
-
-//<div class="player-score" id="score-0">43</div>;
-/*DOM дотроос class, ID-р хайж олохын тулд querySelector-г ашиглана. Мөн ID-р хайж байгаа тохиолдолд getElementById -р хайвал илүү хурдан ажилладаг.  
-window.document.querySelector("#score-0").textContent = dice;
-*/
-
-// document.querySelector("#score-1").innerHTML = "<em>ert</em>";
-
-// Програм эхлэхэд бэлтгэж тоглогчдыэ бүх оноог 0 болгоё
-// document.document.querySelector("#score-0").textContent = 0;
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-
-// Шоог алга болгохын тулд DOM дотроос class-р нь хайж олоод css-н display:none; болгоно.
-document.querySelector(".dice").style.display = "none";
-
-/*Roll dice товчийг дарахад ямар Event /үйл явдал/ болохыг нэмж өгнө. addEventListener-г дуудна. Үүн дотроо 2 argument дамжуулдаг. Эхнийх нь ямар event дээр хариу үйлдэл үзүүлэхийг, дараа нь тухайн эвэнтээр юу хийхээ зааж өгнө. Ж.нь : Хулгана roll dice товчийг click/дарах үйлдэл/ хийхэд ShooShid функц ажиллана.
-
-document.querySelector(".btn-roll").addEventListener("click", shooShid);
-function shooShid() {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  alert("Шоо буулаа: " + diceNumber);
-}
-*/
-// Кодыг олон copy-дохоос зайлсхийж global хувьсагчид оноогоод цаашид хаана ч дуудах боломжтой хялбар болгоё
+// Шооны зургийг үзүүлэх элементийг DOM-оос хайж олоод энд хадгалъя
+// Кодыг олон copy - дохоос зайлсхийж global хувьсагчид оноогоод цаашид хаана ч дуудах боломжтой хялбар болгоё
 var diceDom = document.querySelector(".dice");
-// shooShid функцыг зөвхөн 1 л удаа ашиглаж байгаа тул Anonymous функцаар /нэргүй функцаар/сольё
+
+// Програмыг эхлүүлэх
+initGame();
+
+// Тоглоомыг шинээр эхлэхэд бэлтгэнэ.
+function initGame() {
+  //Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийг 0, 2-р тоглогчийг 1 гэж тэмдэглэе.
+  activePlayer = 0;
+  // Тоглогчдын оноог хадгалдаг хувьсагч
+  scores = [0, 0];
+  // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+  roundScore = 0;
+
+  // Програм эхлэхэд бэлтгэж тоглогчдыэ бүх оноог 0 болгоё
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  // Тоглогчдын нэрийг буцааж гаргах
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  diceDom.style.display = "none";
+}
+
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1 - 6 доторх санамсаргүй нэг тоо гаргаж авна
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   // Шооны зургыг дэлгэцнд харуулна
   diceDom.style.display = "block";
   // Буусан санамсаргүй тоонд харгалзах шооны зургыг вэб дээр гаргаж өгнө.
   diceDom.src = "dice-" + diceNumber + ".png";
+
   // Буусан тоо нь 1-с ялгаатай бол идэвхитэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
   if (diceNumber !== 1) {
     // 1-с ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
@@ -91,6 +99,8 @@ function switchToNextPlayer() {
   // Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
   roundScore = 0;
   document.getElementById("current-" + activePlayer).textContent = 0;
+
+  // Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ.
   activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   // toggle -г ашиглаж улаан цэг байвал хасаж, байхгүй бол нэмдэг болно.
   document.querySelector(".player-0-panel").classList.toggle("active");
@@ -98,3 +108,6 @@ function switchToNextPlayer() {
   // Шоог түр алга болгох
   diceDom.style.display = "none";
 }
+
+// New Game буюу Шинэ тоглоом эхлүүлэх товчны event listener
+document.querySelector(".btn-new").addEventListener("click", initGame);
